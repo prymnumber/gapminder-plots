34887b292c70bba0b8abe9e925f39d985207557f
Irene Chen <chen.irene@gmail.com>
2017-08-06 10:37:14 -0700

    plotted point

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -2,9 +2,10 @@ gapminder <- read.csv("data/gapminder-FiveYearData.csv")
 
 library(ggplot2)
 
-ggplot(
-  gapminder,aes(x=gdpPercap, y=lifeExp))+
-  geom_point(aes(color=continents))
+ggplot(gapminder,aes(x=gdpPercap, y=lifeExp))+
+  geom_point(aes(color=continent))
+
+#geom_smooth(method="lm")
 
 #some comment
 
