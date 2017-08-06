d5cd3c9ea80032116210d980ae0eb14619f1f847
Irene Chen <chen.irene@gmail.com>
2017-08-06 10:15:24 -0700

    changed the color

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -2,8 +2,12 @@ gapminder <- read.csv("data/gapminder-FiveYearData.csv")
 
 library(ggplot2)
 
-ggplot(gapminder,aes(x=gdpPercap, y=lifeExp))+geom_point()
+ggplot(
+  gapminder,aes(x=gdpPercap, y=lifeExp))+
+  geom_point(aes(color=continents))
 
 #some comment
 
 #some more comment
+
+#even more comments
